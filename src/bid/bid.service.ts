89cfbac6413d2a199eb875b45ae12d133acc332b
Sandi-san <nicksrebotsandi@gmail.com>
2024-05-23 19:34:34 +0200

    Changed return order of fetch Bids

--- src/bid/bid.service.ts
+++ src/bid/bid.service.ts
@@ -43,7 +43,7 @@ export class BidService {
           },
         },
         orderBy: {
-          createdAt: 'desc', //order by
+          price: 'desc', //order by
         },
       });
       return bids;
